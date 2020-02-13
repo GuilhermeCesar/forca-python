83bb58a88b4e7432ec5db39e3d92411fada8d4fd
guilherme <gui11_3@hotmail.com>
2020-02-12 22:30:41 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,18 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    #enquanto (True and True)
+    while(not enforcou and not  acertou):
+        print("Jogando........")
+
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
