bd704b79b170839d25749c79cc7863139cc43712
guilherme <gui11_3@hotmail.com>
2020-02-26 07:26:05 -0300

    Adicionar acertos e erros

--- forca.py
+++ forca.py
@@ -3,30 +3,42 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_", "_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     #enquanto (True and True)
     while(not enforcou and not  acertou):
         chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper();
 
         print("Jogando........")
+        if(chute in palavra_secreta):
 
-        index = 0
-        for letra in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
 
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index +1
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+
+                index +=1
+        else:
+            erros +=1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
 
         print(letras_acertadas)
         letras_faltando = str(letras_acertadas.count('_'))
         print('Ainda faltam acertar {} letras'.format(letras_faltando))
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu")
 
     print("Fim do jogo")
 
