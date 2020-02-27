d464f43fbdcb0666ef93d1e171efc7f0d300ff93
guilherme <gui11_3@hotmail.com>
2020-02-26 21:19:03 -0300

    Aprendendo sobre list

--- forca.py
+++ forca.py
@@ -3,8 +3,9 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_","_","_","_","_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
     erros = 0
